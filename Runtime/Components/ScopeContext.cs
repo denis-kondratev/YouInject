7cbed3b0d5508932457080984a1918da243a8577
Denis Kondratev <holygoe@gmail.com>
2022-07-10 20:49:59 +0300

    Implement ScopeContext

---
+++ Runtime/Components/ScopeContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace InjectReady.YouInject
+{
+    [CreateAssetMenu(fileName = "ScopeContext", menuName = "YouInject/Scope Context")]
+    public class ScopeContext : ScriptableObject
+    {
+        public IServiceScope? Scope { get; private set; }
+
+        public void CreateScope()
+        {
+            if (Scope is not null)
+            {
+                throw new InvalidOperationException("Cannot create a scope. The scope already exists.");
+            }
+
+            Scope = Host.RootScope.CreateScope();
+        }
+
+        public void DisposeOfScope()
+        {
+            if (Scope is null)
+            {
+                throw new InvalidOperationException("The scope does not exist.");
+            }
+
+            Scope.DisposeAsync();
+            Scope = null;
+        }
+    }
+}
