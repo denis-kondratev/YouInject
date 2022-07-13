fbff068f1b43580fa2a53ce5dfcfccd36ff1d3b5
Denis Kondratev <holygoe@gmail.com>
2022-07-14 01:22:23 +0300

    Add mock tests

---
+++ Tests/Editor/ServiceCollectionTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace InjectReady.YouInject.Editor.Tests
+{
+    public class ServiceCollectionTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.IsTrue(true);
+            // Use the Assert class to test conditions
+        }
+    }
+}
